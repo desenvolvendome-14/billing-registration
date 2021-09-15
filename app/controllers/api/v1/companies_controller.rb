3841b09efdac8c17c3310bd506115939cc2e2301
Emerson Freitas <mersonsi@gmail.com>
2021-09-15 16:30:12 -0300

    feat: companies controller

---
+++ app/controllers/api/v1/companies_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class CompaniesController < ApplicationController
+      # POST /company
+      def create
+        @company = Company.create(company_params)
+        if @company.save
+          render json: CompanyRepresenter.new(@company).as_json, status: :created
+        else
+          render json: @company.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def company_params
+        params.permit(:company_name)
+      end
+    end
+  end
+end
+
