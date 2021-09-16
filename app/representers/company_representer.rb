c9bd0940a42315c51d3e61892fe353fd4fb25614
Emerson Freitas <mersonsi@gmail.com>
2021-09-16 14:03:04 -0300

    feat: adding the rest of data to representer

--- app/representers/company_representer.rb
+++ app/representers/company_representer.rb
@@ -5,7 +5,11 @@ class CompanyRepresenter
   def as_json
     {
       id: company.id,
-      company_name: company.company_name
+      company_name: company.company_name,
+      cnpj: company.cnpj,
+      state_registration: company.state_registration,
+      fantasy_name: company.fantasy_name,
+      business_phone: company.business_phone
     }
   end
   private
