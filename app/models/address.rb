df303d49b3f394c680fea0d5cf13e4af8ba219c8
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-16 04:40:29 -0400

    Feat models Address

---
+++ app/models/address.rb
@@ -0,0 +1,13 @@
+class Address < ApplicationRecord
+  belongs_to :client
+  belongs_to :carrier
+  belongs_to :seller
+  belongs_to :supplier
+
+  validates :zip_code, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :district, presence: true
+  validates :street, presence: true
+  validates :house_number, presence: true
+end
---
+++ spec/factories/addresses.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :address do
+    zip_code { "MyString" }
+    state { "MyString" }
+    city { "MyString" }
+    district { "MyString" }
+    street { "MyString" }
+    house_number { "MyString" }
+    client { nil }
+    carrier { nil }
+    seller { nil }
+    supplier { nil }
+  end
+end
