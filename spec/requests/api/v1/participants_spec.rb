e7516a62ad51a50cf364ee6464304c0a97e40cf8
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-22 09:24:35 -0400

    Fix Participant

--- spec/requests/api/v1/participants_spec.rb
+++ spec/requests/api/v1/participants_spec.rb
@@ -16,4 +16,53 @@ RSpec.describe "Api::V1::Participants", type: :request do
     end
   end
 
+  describe "POST /participants" do
+    let(:url) { "/api/v1/participants" }
+    let!(:participant) { create(:participant) }
+    let(:participant_params) { attributes_for(:participant) }
+
+    it 'adds a new Participant' do
+      expect do
+        post url, headers: headers, params: { participant: participant_params }
+      end.to change(Participant, :count).by(1)
+    end
+
+    it 'returns success status' do
+      headers = { "ACCEPT" => "application/json" }
+      post url,  headers: headers, params: { participant: participant_params }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "PATCH /participants/:id " do
+    let(:url) { "/api/v1/participants/#{participant.id}" }
+    let!(:participant) { create(:participant) }
+    let(:new_name) { "Goku" }
+    let(:participant_params) { { participant: { name: new_name } }.to_json }
+
+    it 'returns success status' do
+      patch url, headers: headers, params: participant_params
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'DELETE /participants' do
+    let(:url) { "/api/v1/participants/#{participant.id}" }
+    let!(:participant) { create(:participant) }
+
+    it 'removes Participant' do
+      headers = { "ACCEPT" => "application/json" }
+
+      expect do
+        delete url, headers: headers
+      end.to change(Participant, :count).by(-1)
+    end
+
+    it 'returns success status' do
+      headers = { "ACCEPT" => "application/json" }
+
+      delete url, headers: headers
+      expect(response).to have_http_status(:no_content)
+    end
+  end
 end
