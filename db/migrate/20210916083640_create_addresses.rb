54c2f1a93844a0bac3f18c2a654e1e39a2b590f6
Emerson Freitas <mersonsi@gmail.com>
2021-09-22 21:30:17 -0300

    fix: address migration file

--- db/migrate/20210916083640_create_addresses.rb
+++ db/migrate/20210916083640_create_addresses.rb
@@ -7,10 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
       t.string :district
       t.string :street
       t.string :house_number
-      t.references :client, null: false, foreign_key: true
-      t.references :carrier, null: false, foreign_key: true
-      t.references :seller, null: false, foreign_key: true
-      t.references :supplier, null: false, foreign_key: true
+
       t.references :bank, null: false, foreign_key: true
 
       t.timestamps
