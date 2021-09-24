0c625e8bcf440921af11645e10092a9407df94b6
Emerson Freitas <mersonsi@gmail.com>
2021-09-24 19:07:20 -0300

    feat: route for cost center create

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :banks, only: %i[ create ]
       resources :companies, only: %i[ create ], format: "json"
+      resources :cost_centers, only: %i[ create ], format: "json"
     end
-    end
+  end
 
   namespace :api do
     namespace :v1 do
