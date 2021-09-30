47559739aa0f01ffe4a458c166c5185c18205453
Emerson Freitas <mersonsi@gmail.com>
2021-09-30 11:58:06 -0300

    fix: faker cnpj

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :company do
-    cnpj { Faker::CNPJ.pretty }
+    cnpj { Faker::Company.brazilian_company_number(formatted: true) }
     state_registration { Faker::Number.number(digits: 10) }
     company_name { Faker::Company.name }
     fantasy_name { Faker::Company.name }
