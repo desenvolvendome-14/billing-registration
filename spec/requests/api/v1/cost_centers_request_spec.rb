277c70a78313634a22d8a8460f49d5605488e0c1
Emerson Freitas <mersonsi@gmail.com>
2021-10-02 11:28:57 -0300

    test(cost_center): index

--- spec/requests/api/v1/cost_centers_request_spec.rb
+++ spec/requests/api/v1/cost_centers_request_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'Api::V1::CostCenters', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/cost_centers' do
+    context 'with no filters' do
+      before { get '/api/v1/cost_centers' }
+
+      it 'returns cost_centers' do
+        expect(body_json["cost_centers"]).not_to be_empty
+        expect(body_json["cost_centers"].size).to eq(5)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+  end
 end
