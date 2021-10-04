0de188fb339c71c38b05f153b704235cfecb461c
Emerson Freitas <mersonsi@gmail.com>
2021-10-04 10:30:36 -0300

    test(Cost Center): show

--- spec/requests/api/v1/cost_centers_request_spec.rb
+++ spec/requests/api/v1/cost_centers_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::CostCenters', type: :request do
 
   let!(:cost_centers) { create_list(:cost_center, 5) }
+  let!(:cost_center_id) { cost_centers.first.id }
 
   describe 'POST /api/v1/cost_centers' do
     # valid cost center
@@ -36,7 +37,7 @@ RSpec.describe 'Api::V1::CostCenters', type: :request do
     end
 
     context 'with filters' do
-      let!(:cost_center) { create(:cost_center, description: "teste" )}
+      let(:cost_center) { create(:cost_center, description: "teste" )}
 
       before { get "/api/v1/cost_centers?description=#{cost_center.description}" }
 
@@ -46,6 +47,26 @@ RSpec.describe 'Api::V1::CostCenters', type: :request do
         expect(body_json["cost_centers"].size).to eq(1)
       end
     end
+  end
 
+  describe 'GET /cost_centers/:id' do
+    before { get "/api/v1/cost_centers/#{cost_center_id}" }
+    context 'when cost center exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+      it 'returns the cost center item' do
+        expect(body_json['cost_center']['id']).to eq(cost_center_id)
+      end
+    end
+    context 'when cost center does not exist' do
+      let(:cost_center_id) { 0 }
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+      it 'returns a not found message' do
+        expect(response.body).to include("Couldn't find CostCenter with 'id'=0")
+      end
+    end
   end
 end
