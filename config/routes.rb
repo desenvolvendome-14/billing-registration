8310fb52a85e3e80d7620f1644513cc3b97061bb
gustavomendess <gustavocod13@outlook.com>
2021-10-09 16:39:45 -0300

    feat: create and adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :banks, only: %i[ create ]
+      resources :banks, only: %i[ create index destroy ], format: "json"
       resources :companies, only: %i[ create index ], format: "json"
       resources :cost_centers, only: %i[ create index ], format: "json"
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :participants, format: "json"
     end
   end
-  
 end
