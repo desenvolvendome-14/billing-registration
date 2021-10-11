3821fac82d43983694c7773236caf074ffb05a26
Emerson Freitas <mersonsi@gmail.com>
2021-10-10 22:09:51 -0300

    fix(banks): tests

--- app/views/api/v1/banks/index.json.jbuilder
+++ app/views/api/v1/banks/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.banks do
-  json.array! @banks, :id, :code, :description, :created_at, :updated_at, :address_id
+  json.array! @banks, :id, :code, :description, :created_at, :updated_at
 end
--- spec/requests/api/v1/banks_spec.rb
+++ spec/requests/api/v1/banks_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe "Api::V1::Banks", type: :request do
       before { post '/api/v1/banks', params: valid_params }
 
       it 'create a bank' do
-        expect(body_json['code']).to eq(260)
-        expect(body_json['description']).to eq('Nu Payments')
+        expect(body_json['bank']['code']).to eq(260)
+        expect(body_json['bank']['description']).to eq('Nu Payments')
       end
 
       it 'returns status code 201' do
