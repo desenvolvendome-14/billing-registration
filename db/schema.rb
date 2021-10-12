1bb834638a4b6cc6f7f9dd58309c913fb53f75d2
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-10-12 06:03:37 -0300

    feat(document_types): create migrate and update schemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_081754) do
+ActiveRecord::Schema.define(version: 2021_10_12_010923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 2021_09_28_081754) do
   end
 
   create_table "cost_centers", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "document_types", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
