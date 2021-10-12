e5c00279e7b5c88676dccea17d7367d8a14216f2
Emerson Freitas <mersonsi@gmail.com>
2021-10-12 20:41:37 -0300

    chore(charge type): update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_010923) do
+ActiveRecord::Schema.define(version: 2021_10_12_233448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2021_10_12_010923) do
 
   create_table "banks", force: :cascade do |t|
     t.integer "code"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "charge_types", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
