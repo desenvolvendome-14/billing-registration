bdf1f2e80d02c7273f5fe23b9563c7a9b9eaffe1
Emerson Freitas <mersonsi@gmail.com>
2021-10-13 06:10:28 -0300

    fix(cost centers): missing tests

--- app/controllers/api/v1/cost_centers_controller.rb
+++ app/controllers/api/v1/cost_centers_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class CostCentersController < ApplicationController
-      # POST api/v1/cost center
+      before_action :set_cost_center, only: %w[show update destroy]
+
+      # POST api/v1/cost_centers
       def create
         @cost_center = CostCenter.create(cost_center_params)
         if @cost_center.save
@@ -18,9 +20,23 @@ module Api
         render :index
       end
 
+      def update
+        @cost_center.update(cost_center_params)
+        head :no_content
+      end
+      # DELETE /cost_centers/:id
+      def destroy
+        @cost_center.destroy
+        head :no_content
+      end
+
       private
       def cost_center_params
         params.permit(:description)
+      end
+
+      def set_cost_center
+        @cost_center = CostCenter.find(params[:id])
       end
     end
   end
