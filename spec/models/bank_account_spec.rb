ba5e9cc0b45aa4ba02b29c7536b603c50e0f48bc
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-10-13 23:30:10 -0300

    test(bank_account): validates

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,7 +1,9 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe BankAccount, type: :model do
-  it "create charts account with factory default values" do
+  it "create bank account with factory default values" do
     bank_account = build(:bank_account)
     expect(bank_account.description).to eq "conta corrente"
     expect(bank_account.city).to eq "Coroados-SP"
@@ -12,9 +14,9 @@ RSpec.describe BankAccount, type: :model do
     expect(bank_account.assignor).to eq "77777"
   end
 
-  it "create charts account with factory other values" do
-    bank_account = build(:bank_account, description: "poupança", city: "São Paulo-SP", agency: "0002", agency_digit: "2",
-                         account: "8888", account_digit: "8", assignor: "66666")
+  it "create bank account with factory other values" do
+    bank_account = build(:bank_account, description: "poupança", city: "São Paulo-SP", agency: "0002",
+                                        agency_digit: "2", account: "8888", account_digit: "8", assignor: "66666")
     expect(bank_account.description).to eq "poupança"
     expect(bank_account.city).to eq "São Paulo-SP"
     expect(bank_account.agency).to eq "0002"
@@ -23,4 +25,9 @@ RSpec.describe BankAccount, type: :model do
     expect(bank_account.account_digit).to eq "8"
     expect(bank_account.assignor).to eq "66666"
   end
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:city) }
+  it { is_expected.to validate_presence_of(:agency) }
+  it { is_expected.to validate_presence_of(:account) }
+  it { is_expected.to validate_presence_of(:assignor) }
 end
