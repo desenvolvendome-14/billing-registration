129b657035ca664c02be60c32d80c363f6b435fd
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-10-28 05:34:16 -0300

    feat(participants): validates

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,5 @@
 class Participant < ApplicationRecord
-  validates :name, presence: true
+  validates :name, length: { minimum: 3 }, presence: true
   validates :cpf_cnpj, presence: true
   validates :fantasy_name, presence: true
   validates :person_type, presence: true
@@ -7,5 +7,4 @@ class Participant < ApplicationRecord
 
   enum person_type: {legal_person: 1, physical_person: 2, foreign:3}
   enum client_type: {customer: 1, supplier: 2, seller:3, carrier:4}
-
 end
