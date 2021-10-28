a1aa6b6224a005afbdc369dd5ed7a7cf51df4221
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-10-28 06:24:09 -0300

    test(participant): association

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -14,5 +14,6 @@ RSpec.describe Participant, type: :model do
   it { should validate_length_of(:name).is_at_least(3) }
 
   it { should have_many(:contacts) }
+  it { should have_many(:addresses) }
 end
 
