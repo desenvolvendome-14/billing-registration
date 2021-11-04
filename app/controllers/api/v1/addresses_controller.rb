71cc9501298dab19ce0d7d79c9ca6650d50dd5b4
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-11-04 05:28:01 -0300

    feat(addresses): created update

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class AddressesController < ApplicationController
+      before_action :set_address, only: %i[update]
+
+
       def index
         @addresses = Address.all
       end
@@ -15,7 +18,21 @@ module Api
         end
       end
 
+      def update
+        if @address.update(address_params)
+          render json: @address, status: :ok
+        else
+          render json: @address.errors, status: :unprocessable_entity
+        end
+      end
+
       private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_address
+        @address = Address.find(params[:id])
+      end
+
       def address_params
         params.require(:address).permit(:zip_code, :state, :city, :district, :complement, :street, :house_number,
                                         :company_id, :participant_id )
