bcf5a3b04628a4dabf508acc3c2eb14ec9075abe
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-11-05 06:00:10 -0300

    test(address): create address with other values

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -17,7 +17,21 @@ RSpec.describe Address, type: :model do
     expect(address.street).to eq("Rua Maestro Antônio Guerra")
     expect(address.house_number).to eq("39")
     expect(address.complement).to eq("casa")
-    expect(address.participant_id).to eq("1")
+    expect(address.participant_id).to eq(1)
+  end
+
+  it "create charts account with factory other values" do
+    address = build(:address, zip_code: "16200203", state: "SP", city: "Birigui", district: "Vila Angélica",
+                    street: "Avenida São Francisco", house_number: "156", complement: "predio", participant_id: "2" )
+
+    expect(address.zip_code).to eq("16200203")
+    expect(address.state).to eq("SP")
+    expect(address.city).to eq("Birigui")
+    expect(address.district).to eq("Vila Angélica")
+    expect(address.street).to eq("Avenida São Francisco")
+    expect(address.house_number).to eq("156")
+    expect(address.complement).to eq("predio")
+    expect(address.participant_id).to eq(2)
   end
 end
 
