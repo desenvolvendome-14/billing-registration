8cdc5522426d625e34ca3ef64da7a6b1e134b01f
Emerson Freitas <mersonsi@gmail.com>
2021-11-05 18:30:12 -0300

    feat(participants): validate cpf_cnpj

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,6 +4,7 @@ class Participant < ApplicationRecord
   validates :cpf_cnpj, presence: true
   validates :person_type, presence: true
   validates :client_type, presence: true
+  validate :validate_cpf_cnpj
 
   enum person_type: {legal_person: 1, physical_person: 2, foreign:3}
   enum client_type: {customer: 1, supplier: 2, seller:3, carrier:4}
@@ -14,6 +15,12 @@ class Participant < ApplicationRecord
 
   def fantasy_name_nil?
     self.fantasy_name = name
+  end
+
+  def validate_cpf_cnpj
+    unless CPF.valid?(cpf_cnpj) || CNPJ.valid?(cpf_cnpj)
+      errors.add(:cpf_cnpj, :invalid)
+    end
   end
 
 end
