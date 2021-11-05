c88dee8e848b461fb85f0107a5e083a44e68c3bd
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-11-05 05:31:57 -0300

    test(address): created with factory values

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Address, type: :model do
   it { is_expected.to validate_presence_of(:street) }
   it { is_expected.to validate_presence_of(:house_number) }
 
+  it "create charts account with factory default values" do
+    address = build(:address)
+    expect(address.zip_code).to eq("16260000")
+    expect(address.state).to eq("SP")
+    expect(address.city).to eq("Coroados")
+    expect(address.district).to eq("Gentil Bernardes")
+    expect(address.street).to eq("Rua Maestro Antônio Guerra")
+    expect(address.house_number).to eq("39")
+    expect(address.complement).to eq("casa")
+    expect(address.participant_id).to eq(1)
+  end
 end
