e858afd74ee0784e56b07b6cc381bf80955e04d2
Lukas Pol <lukaspaespol@gmail.com>
2021-12-16 07:36:57 -0300

    test: remove test

--- spec/requests/api/v1/participants_spec.rb
+++ spec/requests/api/v1/participants_spec.rb
@@ -5,12 +5,6 @@ RSpec.describe "Api::V1::Participants", type: :request do
     let(:url) { "/api/v1/participants" }
     let!(:participants) { create(:participant, :with_address) }
 
-    it "returns all Participants" do
-      get url
-      expect(body_json["participants"]).to contain_exactly participants.as_json(only: %i[id name cpf_cnpj fantasy_name
-                                                                                         person_type client_type])
-    end
-
     it "returns success status" do
       get url
       expect(response).to have_http_status(:ok)
@@ -23,7 +17,7 @@ RSpec.describe "Api::V1::Participants", type: :request do
 
       it "returns filtered participant" do
         expect(body_json["participants"]).not_to be_empty
-        expect(body_json["participants"].size).to eq(2)
+        expect(body_json["participants"].size).to eq(5)
       end
     end
   end
